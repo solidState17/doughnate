f510b894ab1e1bf1c3a2b26af852d6a66073837a
Seth Wright <seth@Seths-MacBook.local>
2021-03-04 11:58:31 +0900

    Added 'AppSettings' and 'Friends' pages, completed bottom navbar and buttons.

---
+++ lib/appsettings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppSettings extends StatefulWidget {
+  AppSettings({Key key}) : super(key: key);
+  @override
+  _AppSettings createState() => _AppSettings();
+}
+
+class _AppSettings extends State<AppSettings> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("AppSettings View ~!!!!!!!");
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,72 +1,55 @@
 import 'package:flutter/material.dart';
-import './login.dart';
+// import './login.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'appsettings.dart';
+import 'friends.dart';
 
-class Home extends StatefulWidget{
-  
+class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
-
 }
 
-class _HomeState extends State<Home>{
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
 
-    int _currentIndex = 0;
-
-  Widget build(BuildContext context){
-  return Scaffold(
-    body: Container(
-      child:Center(
-        child:Column(
-          children: <Widget>[
-            Text("This is Home Page!"),
-            RaisedButton(
-              child: const Text('Log out'),
-              color: Colors.blue,
-              shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            onPressed: () {Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => GoogleAuth())
-                      );},
-          ),
-          ],
-        ),
-      ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex:_currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title:Text("Home"),
-            backgroundColor:Colors.blue
-            ),
-
-            BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            title:Text("Search"),
-            backgroundColor:Colors.blue
-            ),
-
-            BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            title:Text("Home"),
-            backgroundColor:Colors.blue
-            ),
-
-            BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title:Text("Home"),
-            backgroundColor:Colors.blue
-            ), 
-        ],
-        onTap: (index) {
-          setState((){
-            _currentIndex = index;
-          });
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () async {
+          return false;
         },
-      ),
-    );
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Doughnate'),
+            automaticallyImplyLeading: false,
+          ),
+          // this is the home page
+          body: Container(
+            child:
+                Center(child: _currentIndex == 2 ? AppSettings() : Friends()),
+          ),
+
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  title: Text("Home"),
+                  backgroundColor: Colors.blue),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.add_outlined),
+                  title: Text("Add"),
+                  backgroundColor: Colors.blue),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  title: Text("Settings"),
+                  backgroundColor: Colors.blue),
+            ],
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+        ));
   }
 }
