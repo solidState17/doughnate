ac079269bf3faf57257151dab6dc8b7231481ce4
Gukou4869 <st11121.grtkmch@gmail.com>
2021-03-07 13:19:50 +0900

    Created search page

--- lib/search.dart
+++ lib/search.dart
@@ -1,35 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/rendering.dart';
+import './login.dart';
 
-class Search extends StatelessWidget {
+ var friendUserPic = "";
+ var friendUserName = "";
+
+class Search extends StatefulWidget {
+  @override
+  _SearchTextFieldState createState() => _SearchTextFieldState();
+
+}
+
+class _SearchTextFieldState extends State<Search>{
+  final _controller = TextEditingController();
+  final FirebaseFirestore fireStore = FirebaseFirestore.instance;
+  String name = "";
+  var index = 0;
+  final pages = [
+    DefaultPage(),
+    FriendsInfo(),
+    ErrorMassage()
+  ];
+
+  searchFriends(email) async{
+  await fireStore
+   .collection("users")
+   .where("email", isEqualTo: name)
+   .get()
+   .then((value) {
+     setState(() {
+       index = 1;
+       friendUserPic = value.docs[0].data()["profilePic"];
+       friendUserName = value.docs[0].data()["displayName"];
+     });
+     print("ユーザーはいるよ。");
+   })
+   .catchError((err) {
+    setState(() {
+       index = 2;
+     });
+     print("Invalid Email Adress");
+        });
+  }
    Widget build(BuildContext context){
      return Scaffold(
        appBar: AppBar(
-         title: Text("Search"),
+         title: Text("Search Friends"),
+       ),
+       body: Column(
+         children: [
+           Row(
+           children: [
+             Container(
+             padding: EdgeInsets.only(top: 10, right: 20, bottom: 30, left: 20),
+             child: TextField(
+               controller: _controller,
+               onChanged: (x) => name = x,
+               decoration: InputDecoration(
+                 enabledBorder: OutlineInputBorder(
+                   borderSide: BorderSide(color: Colors.transparent),
+                   borderRadius: BorderRadius.all(Radius.circular(10))
+                 ),
+                 focusedBorder: OutlineInputBorder(
+                   borderSide: BorderSide(color:Colors.transparent),
+                   borderRadius:BorderRadius.all(Radius.circular(10))
+                 ),
+                 hintText: "Email",
+                 filled: true,
+               ),
+             ),
+           ),
+           Container(
+             
+           )
+           ],
+           ),
+           Container(
+             color: Colors.blue,
+             child: TextButton(
+               onPressed: (){searchFriends(name);},
+               child: Text(
+                 "Search", style: TextStyle(
+                   color:Colors.white,
+                 ),
+               )
+             )
+           ),
+           Container(
+             child: Center(
+               child: pages[index]
+             )
+           )
+         ],
        ),
      );
    }
 }
 
-// TextEditingController email = TextEditingController();
-//                 showDialog(
-//                     context: context,
-//                     builder: (context) => new AlertDialog(
-//                             title: new Text("Enter an email address:"),
-//                             content: new TextFormField(
-//                               controller: _email,
-//                               decoration: const InputDecoration(
-//                                 labelText: "email",
-//                                 hintText: 'Enter an email',
-//                               ),
-//                             ),
-//                             actions: <Widget>[
-//                               new TextButton(
-//                                   onPressed: () {
-//                                     handleAddingFriend();
-//                                     getAllFriends();
-//                                   },
-//                                   child: new Text("Add"))
-//                             ],
-                            
-//                             )
-//                             );
+class ErrorMassage extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal:30, vertical:30),
+        child: Text(
+        "Inputed Email Address is not registered in Doughnate",
+        style: TextStyle(
+          fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey
+        ),),
+    );
+  }
+}
+
+class FriendsInfo extends StatelessWidget{
+  Widget build(BuildContext context){
+    return  Column(
+      children:[
+      Container(
+        padding: EdgeInsets.symmetric(horizontal:30, vertical:30),
+        child:CircleAvatar(
+          radius: 60, 
+          child: ClipOval(
+            child: Image(
+            image: NetworkImage(
+              friendUserPic
+             ),
+            ),
+          ),
+        ),
+      ), 
+      Container(
+        child:Text(
+          friendUserName,
+          style: TextStyle(
+            fontSize: 20, fontWeight: FontWeight.bold
+          ),
+        )
+      ),
+      Container(
+        margin: EdgeInsets.only(top:20),
+        width:120,
+        color: Colors.green,
+        child:TextButton(
+          onPressed: (){},
+          child: Text(
+            "Add",
+            style: TextStyle(
+              color:Colors.white,
+            ),
+          )
+          )
+      )
+      ]
+    );
+  }
+}
+
+class DefaultPage extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
