af918683583e27c551849a68981559eee9368494
ntaylor6422 <ntaylor6422@gmail.com>
2021-03-08 13:57:42 +0900

    Fixed merge conflict

--- lib/home.dart
+++ lib/home.dart
@@ -11,7 +11,7 @@ import 'search.dart';
 var friends = [];
 
 class Home extends StatefulWidget {
-  String value;
+  final String value;
   Home({this.value});
 
   @override
@@ -30,11 +30,7 @@ class _HomeState extends State<Home> {
         .where("email", isEqualTo: _email.text)
         .get()
         .then((value) {
-<<<<<<< HEAD
       if (value.docs.length > 0) {
-=======
-      if(value.docs.length > 0){
->>>>>>> 8689da9a14362296a0fda0238e40ab915b317cc9
         print("You have this friend already!");
         return;
       }
@@ -75,7 +71,7 @@ class _HomeState extends State<Home> {
       var user = friend['userA'] == email ? friend['userB'] : friend['userA'];
 
       final pulledUser =
-      await db.collection("users").where("email", isEqualTo: user).get();
+          await db.collection("users").where("email", isEqualTo: user).get();
 
       final listUser = pulledUser.docs[0].data();
       listUser['friendship'] = friend;
@@ -106,7 +102,7 @@ class _HomeState extends State<Home> {
           // this is the home page
           body: Container(
             child:
-            Center(child: _currentIndex == 2 ? AppSettings() : Friends()),
+                Center(child: _currentIndex == 2 ? AppSettings() : Friends()),
           ),
 
           bottomNavigationBar: BottomNavigationBar(
@@ -134,17 +130,11 @@ class _HomeState extends State<Home> {
               });
               if (_currentIndex == 1) {
                 Navigator.push(
-<<<<<<< HEAD
                     context,
                     MaterialPageRoute(
                       builder: (context) => Search(),
                       fullscreenDialog: true,
                     ));
-=======
-                    context, MaterialPageRoute(
-                  builder: (context) => Search(),
-                  fullscreenDialog: true,));
->>>>>>> 8689da9a14362296a0fda0238e40ab915b317cc9
               }
             },
             selectedItemColor: const Color(0xff4d4d4d),
